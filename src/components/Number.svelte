456e703c9dea5997c6b44b11dcaccd685bd25f26
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-12 21:46:35 +0300

    Add `Number component

---
+++ src/components/Number.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let number = 1;
+  export let editable = false;
+
+  const dispatch = createEventDispatcher()
+  $: {
+    dispatch('change', { number })
+  }
+</script>
+
+<style>
+  .number {
+    border-radius: 1px;
+    display: block;
+    padding: 10px;
+    background: #fff;
+    margin: 10px 0;
+  }
+  .number-input {
+    background: none;
+    text-align: center;
+    border: 0;
+  }
+</style>
+
+<div class="number">
+  {#if editable}
+    <input type="text" class="number-input" bind:value={number} />
+  {:else}
+    {number}
+  {/if}
+</div>
