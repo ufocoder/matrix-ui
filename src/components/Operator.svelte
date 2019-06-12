02d67ab21a524b2b8fd66379e605bee49ccdffbf
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-12 21:46:47 +0300

    Add `Operator` component

---
+++ src/components/Operator.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let operator = "x"
+</script>
+
+<style>
+  .operator {
+    opacity: 0.8;
+    padding: 10px;
+    margin: 10px;
+    user-select: none;
+    font-size: 40px;
+  }
+</style>
+
+<div class="operator">
+  {operator}
+</div>
