3851179f99f76ad2e439b8aacb54a8acc73a75e7
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 15:56:58 +0300

    Improve matrix generator and add test

--- src/lib/generator.ts
+++ src/lib/generator.ts
@@ -11,7 +11,8 @@ export const CellInitiator = {
     diagonal: (x, y) => x === y ? generateNumber(-100, 100) : 0,
     logical: () => generateNumber(0, 1),
     nonnegative: () => generateNumber(0, 100),
-    incremental: (x, y, { width }) => x + y * width + 1
+    incremental: (x, y, { width }) => x + y * width + 1,
+    reverseIncremental: (x, y, { height }) => x * height + y + 1
 }
 
 const createBasicGenerator = (cellInitiator: Initiator) => (width: number, height: number) => (
@@ -23,15 +24,18 @@ const createSquareGenerator = (cellInitiator) => (size) => (
 )
 
 export const zero = createBasicGenerator(CellInitiator.zero)
-export const identity = createSquareGenerator(CellInitiator.identity)
-export const incremental = createSquareGenerator(CellInitiator.incremental)
 export const logical = createBasicGenerator(CellInitiator.logical)
 export const nonnegative = createBasicGenerator(CellInitiator.nonnegative)
+export const incremental = createBasicGenerator(CellInitiator.incremental)
+export const reverseIncremental = createBasicGenerator(CellInitiator.reverseIncremental)
+
+export const identity = createSquareGenerator(CellInitiator.identity)
 
 export default {
     zero,
     identity,
     incremental,
+    reverseIncremental,
     logical,
     nonnegative
 }
---
+++ tests/lib/generator.spec.ts
@@ -0,0 +1,54 @@
+import Generator from '../../src/lib/generator'
+
+describe('Generator', () => {
+  it('identity', () => {
+    const size = 4
+    const matrix = Generator.identity(size);
+    const items = matrix.getItems()
+
+    expect(items).toEqual([
+      [1, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 1]
+    ])
+  })
+  it('zero', () => {
+    const width = 4
+    const height = 4
+    const matrix = Generator.zero(width, height);
+    const items = matrix.getItems()
+
+    expect(items).toEqual([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ])
+  })
+  it('incremental', () => {
+    const width = 4
+    const height = 2
+    const matrix = Generator.incremental(width, height);
+    const items = matrix.getItems()
+    console.log(items);
+    
+    expect(items).toEqual([
+      [1, 2, 3, 4],
+      [5, 6, 7, 8]
+    ])
+  })
+  it('reverse incremental', () => {
+    const width = 2
+    const height = 4
+    const matrix = Generator.reverseIncremental(width, height);
+    const items = matrix.getItems()
+    
+    expect(items).toEqual([
+      [1, 5],
+      [2, 6],
+      [3, 7],
+      [4, 8]
+    ])
+  })
+})
