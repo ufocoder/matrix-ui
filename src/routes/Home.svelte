cadfda1507fbc99c7ea1c60e2408981b43bd70d6
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 06:19:23 +0300

    Add keyboard help

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,30 @@
-Sweet home
+<h1>Math UI</h1>
+
+
+<h3>Keyboard control</h3>
+
+<h4>Matrix cell navigation</h4>
+<div>
+  <span class="label">⌘</span> + <span class="label">↑</span>
+  Move focus previous row cell
+</div>
+<div>
+  <span class="label">⌘</span> + <span class="label">↓</span>
+  Move focus next row cell
+</div>
+<div>
+  <span class="label">⌘</span> + <span class="label">→</span>
+  Move focus next column cell
+</div>
+<div>
+  <span class="label">⌘</span> + <span class="label">←</span>
+  Move focus previous column cell
+</div>
+
+<h4>Number input manipulation</h4>
+<div>
+  <span class="label">↑</span> - increase value
+</div>
+<div>
+  <span class="label">↓</span> - decrease value
+</div>
