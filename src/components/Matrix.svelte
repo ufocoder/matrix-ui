09ee6bdb60acd7b7a297358e016342b08f5946dc
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 05:23:09 +0300

    Fix and Improve `Matrix` component

--- src/components/Matrix.svelte
+++ src/components/Matrix.svelte
@@ -1,55 +1,25 @@
 <script>
-  import Input from 'src/components/Input'
+  import { createEventDispatcher } from 'svelte';
   import Matrix from 'src/lib/matrix'
 
   export let editable = false
   export let fixed = false
-  export let matrix = new Matrix()
+  export let data = [[1,2],[3,4]]
 
-  let tableElement
-
-  $: rows = matrix.getRows()
-
+  const dispatch = createEventDispatcher()
+  
   const createClickHandler = (modifyMatrix) => () => {
-      matrix = modifyMatrix()
+    matrix = modifyMatrix()
+    data = matrix.getData()
+    dispatch('change', { data })
   }
 
-  const createHandleKeydown = (prevRow, prevCol) => (e) => {
-      let nextRow = prevRow
-      let nextCol = prevCol
-      let stopPropagation = false
+  let matrix
+  let rows
 
-      if ('ArrowUp' === e.code && e.metaKey) {
-          nextRow = Math.max(0, prevRow - 1)
-          stopPropagation = true
-      }
-
-      if ('ArrowDown' === e.code && e.metaKey) {
-          nextRow = Math.min(matrix.height, prevRow + 1)
-          stopPropagation = true
-      }
-
-      if ('ArrowLeft' === e.code && e.metaKey) {
-          nextCol = Math.max(0, prevCol - 1)
-          stopPropagation = true
-      }
-
-      if ('ArrowRight' === e.code && e.metaKey) {
-          nextCol = Math.min(matrix.width, prevCol + 1)
-          stopPropagation = true
-      }
-
-      if (stopPropagation) {
-          e.stopImmediatePropagation()
-      }
-
-      if (prevRow !== nextRow || prevCol !== nextCol) {
-          const element = tableElement.querySelector(`tr:nth-of-type(${nextRow}) td:nth-of-type(${nextCol}) input`)
-
-          if (element) {
-              element.focus()
-          }
-      }
+  $: {
+    matrix = new Matrix(data)
+    rows = matrix.getRows()
   }
 </script>
 
@@ -59,20 +29,24 @@
     display: inline-block;
     margin: 30px;
   }
+
   .matrix-table {
     position: relative;
     padding: 0;
     border-spacing: 0;
     border-collapse: separate;
     background: #fff;
+    /* box-shadow: 0 10px 29px 0 rgba(68, 88, 144, 0.1); */
   }
+
   .matrix-cell {
     border-left: 1px solid #eee;
     padding: 0;
     margin: 0;
-    min-width: 50px;
-    line-height: 50px;
+    min-width: 40px;
+    line-height: 40px;
     text-align: center;
+
   }
   .matrix-cell:last-child {
     border-right: 1px solid #eee;
@@ -83,6 +57,14 @@
   }
   .matrix-row:first-child {
     border-top: 1px solid #eee;
+  }
+  .matrix-cell__input {
+    margin: 0;
+    text-align: center;
+    padding: 10px;
+    border: none;
+    width: 100%;
+    height: 100%;
   }
   .matrix__create-row {
     position: absolute;
@@ -97,6 +79,7 @@
   } 
   .matrix__create-row--below {
     bottom: -30px;
+    
   } 
   .matrix__create-column {
     text-align: center;
@@ -123,43 +106,41 @@
 </style>
 
 <div class="matrix">
-  {#if matrix}
-    {#if editable}
-      <div 
-        class="matrix__create-row  matrix__create-row--above" 
-        on:click={createClickHandler(matrix.addRowAbove)}>
-        + add row
-      </div>
-      <div 
-        class="matrix__create-column matrix__create-column--left" 
-        on:click={createClickHandler(matrix.addColumnLeft)}>
-        + add column
-      </div>
-      <div 
-        class="matrix__create-column matrix__create-column--right" 
-        on:click={createClickHandler(matrix.addColumnRight)}>
-        + add column
-      </div>
-      <div
-        class="matrix__create-row matrix__create-row--below" 
-        on:click={createClickHandler(matrix.addRowBelow)}>
-        + add row
-      </div>
-    {/if}
-    <table class="matrix-table" bind:this={tableElement}>
-      {#each rows as row, i}
-        <tr class="matrix-row">
-            {#each row as number, j}
-              <td class="matrix-cell">
-                {#if editable}
-                  <Input bind:value={number} on:keydown={createHandleKeydown(i + 1, j + 1)} />
-                {:else}
-                  {number}
-                {/if}
-              </td>
-            {/each}
-        </tr>
-      {/each}
-    </table>
+  {#if editable}
+    <div 
+      class="matrix__create-row  matrix__create-row--above" 
+      on:click={createClickHandler(matrix.addRowAbove)}>
+      add row
+    </div>
+    <div 
+      class="matrix__create-column matrix__create-column--left" 
+      on:click={createClickHandler(matrix.addColumnLeft)}>
+      add column
+    </div>
+    <div 
+      class="matrix__create-column matrix__create-column--right" 
+      on:click={createClickHandler(matrix.addColumnRight)}>
+      add column
+    </div>
+    <div
+      class="matrix__create-row matrix__create-row--below" 
+      on:click={createClickHandler(matrix.addRowBelow)}>
+      add row
+    </div>
   {/if}
+  <table class="matrix-table">
+		{#each rows as row}
+			<tr class="matrix-row">
+					{#each row as value}
+						<td class="matrix-cell">
+              {#if editable}
+                <input type="text" class="matrix-cell__input" bind:value={value} />
+              {:else}
+                {value}
+              {/if}
+						</td>
+					{/each}
+			</tr>
+		{/each}
+	</table>
 </div>
