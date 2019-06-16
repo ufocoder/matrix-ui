35d9156d897e41f9d4e67d08da84bd3bee7cfe2f
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 05:20:01 +0300

    Create `Input` component

---
+++ src/components/Input.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { afterUpdate } from 'svelte';
+
+  export let value = 1;
+
+  let element
+  let selectionStart
+  let selectionEnd
+
+  afterUpdate(() => {
+      element.setSelectionRange(selectionStart, selectionEnd);
+  })
+
+  const handleKeydown = function (e) {
+      selectionStart = this.selectionStart
+      selectionEnd = this.selectionEnd
+
+      if ('ArrowUp' === e.code) {
+          value += 1
+          e.preventDefault()
+      }
+
+      if ('ArrowDown' === e.code) {
+          value -= 1
+          e.preventDefault()
+      }
+  }
+</script>
+
+<style>
+  .input {
+    background: none;
+    text-align: center;
+    border: 0;
+    padding: 10px;
+    height: 50px;
+  }
+  .input:focus {
+    outline-color: #807fe2;
+    outline-style: solid;
+    outline-width: 2px;
+  }
+</style>
+
+<input 
+  class="input" 
+  type="text" 
+  bind:this={element} 
+  bind:value={value} 
+  on:keydown
+  on:keydown={handleKeydown} />
