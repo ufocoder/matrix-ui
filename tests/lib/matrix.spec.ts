31790199b29881ee0c0bcfd93a117d31dcc45978
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 17:08:17 +0300

    Improve matrix spec

--- tests/lib/matrix.spec.ts
+++ tests/lib/matrix.spec.ts
@@ -7,22 +7,21 @@ describe('Matrix', () => {
     expect(matrix.width).toBe(2)
     expect(matrix.height).toBe(3)
 
-    for (let i = 0; i < matrix.height; i++) {
-      for (let j = 0; j < matrix.width; j++) {
-        expect(matrix.getCell(i, j)).toBe(4)
-      }
-    }
+    expect(matrix.getItems()).toEqual([
+      [4, 4],
+      [4, 4],
+      [4, 4]
+    ])
   })
 
   it('multiply on number', () => {
     const matrix = Matrix.create(2, 3, 4);
-    const modified = matrix.multiply(5)
 
-    for (let i = 0; i < modified.height - 1; i++) {
-      for (let j = 0; j < modified.width - 1; j++) {
-        expect(modified.getCell(i, j)).toBe(20)
-      }
-    }
+    expect(matrix.multiply(5).getItems()).toEqual([
+      [20, 20],
+      [20, 20],
+      [20, 20]
+    ])
   })
 
   it('multiply on matrix', () => {
