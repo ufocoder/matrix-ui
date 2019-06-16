16f4bd8932991ee844725987498b415519270756
Sergey Ufocoder <xufocoder@gmail.com>
2019-06-16 05:24:14 +0300

    Add `MultiplyMatrix` component-route

---
+++ src/routes/MultiplyMatrix.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Generators from 'src/lib/generator'
+  import MatrixComponent from 'src/components/Matrix'
+  import OperatorComponent from 'src/components/Operator'
+
+  let matrixA = Generators.incremental(3)
+  let matrixB = Generators.identity(3)
+
+  $: canBeMultiplied = matrixA.canMultiplyOnMatrix(matrixB)
+  $: matrixC = canBeMultiplied ? matrixA.multiply(matrixB) : null
+
+</script>
+
+<style>
+.expression {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.expression-operator {
+  flex: 1;
+  margin: 0 20px;
+}
+.expression-operand {
+  flex: 1;
+}
+</style>
+
+<h2>Multiply matrix on matrix </h2>
+
+<div class="expression">
+  <div class="expression-operand">
+    <MatrixComponent bind:matrix={matrixA} editable />
+  </div>
+  <div class="expression-operator">
+    <OperatorComponent operator="x" />
+  </div>
+  <div class="expression-operand">
+    <MatrixComponent bind:matrix={matrixB} editable />
+  </div>
+
+  <div class="expression-operator">
+    <OperatorComponent operator="=" />
+  </div>
+  <div class="expression-operand">
+    {#if canBeMultiplied}
+      <MatrixComponent matrix={matrixC} />
+    {:else} 
+      Not allowed
+    {/if}
+  </div>
+</div>
